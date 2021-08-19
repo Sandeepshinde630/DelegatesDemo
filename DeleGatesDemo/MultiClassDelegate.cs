332588d2e793973ca297923a515e42816b08e9e4
unknown <sandeepshinde630@gmail.com>
2021-08-19 17:58:35 +0530

    feat:Added Dlegates Demo Code

---
+++ DeleGatesDemo/MultiClassDelegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleGatesDemo
+{
+    public class MultiClassDelegate
+    {
+        delegate void DelOp(int x, int y);
+        public static void ImplementDelegate()
+        {
+            DelOp obj = Operation.Add;
+            obj += Operation.Square;
+
+            obj(5, 3);
+            obj(5, 6);
+            Console.ReadLine();
+        }
+        public class Operation
+        {
+            public static void Add(int a, int b)
+            {
+                Console.WriteLine("Add={0}", a + b);
+            }
+            public static void Square(int a, int b)
+            {
+                Console.WriteLine("Multiple={0}", a * b);
+            }
+        }
+    }
+}
---
+++ DeleGatesDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DeleGatesDemo
+{
+    public delegate int Opertation(int x, int y);
+    class Program
+    {
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate Demo!");
+            Opertation obj = new Opertation(Addition);
+            Console.WriteLine("Addition is ={0}", obj(20, 30));
+
+            MultiClassDelegate.ImplementDelegate();
+        }
+    }
+}
